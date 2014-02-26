cac8b1323b22541b3116e37e733d3af43b15c249
Erica Pisani <pisani.erica@gmail.com>
2014-02-26 13:35:31 -0500

    Parse and return JSON string

--- infected_comp.py
+++ infected_comp.py
@@ -1,7 +1,37 @@
+#!/usr/bin/env python
+#Author: Erica Pisani
+
+import collections
 import json
+import csv
 
-class InfectedComputerParser():
+def parseThatShit(fileNameToParse):
 
-	def setUp():
-		# import file here
+	# Used to skip the first row in the file when parsing
+	# for the information
+	rowCount = 0
 
+	jsonString = "AntiVirus_Reports: [\n"
+	# Opening in universal newline mode. Ensures backward
+	# compatibility.
+	file = open(fileNameToParse, "U")
+
+	fileLen = len(open(fileNameToParse, "r").readlines())
+
+	colNames = csv.reader(file)
+	headings = colNames.next()
+
+	information = csv.DictReader(file, headings)
+	for row in information:
+		row = collections.OrderedDict(sorted(row.items()))
+		jsonString += json.dumps(row)
+		rowCount +=1
+		if rowCount == (fileLen - 1):
+			jsonString += "\n"
+		else:
+			jsonString += ",\n"
+
+	jsonString += "]"
+
+	print jsonString
+	return jsonString
