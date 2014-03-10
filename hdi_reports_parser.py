e76c76e70ee19776c4b5904f4111c902c4e794e9
Sam Wilkins <swilkins1337@gmail.com>
2014-03-10 15:00:37 -0400

    Attempt to match required JSON format. Still need to deal with dates appearing as floats.

--- hdi_reports_parser.py
+++ hdi_reports_parser.py
@@ -5,25 +5,35 @@
 import sys
 import xlrd
 import json
+import time
 
-if len(sys.argv) < 2:
-	sys.exit('No input file')
+def main():
+	if len(sys.argv) < 2:
+		sys.exit('No input file')
 
-filename = sys.argv[1]
+	filename = sys.argv[1]
 
-HDI_book = xlrd.open_workbook(filename = filename)
+	HDI_book = xlrd.open_workbook(filename = filename)
+	result_dict = { 'tablename' : 'HDI_Reports',
+					'timestamp' : time.strftime("%Y:%m:%d %H:%M:%S", time.localtime(time.time()))
+				  }
+	rows = list()
 
-HDI_sheet = HDI_book.sheet_by_name('anonymized data')
+	for sheet_index in xrange(0, HDI_book.nsheets):
+		HDI_sheet = HDI_book.sheet_by_index(sheet_index)
 
-rows = list()
-head_row = HDI_sheet.row(0)
-head_values = [cell.value for cell in head_row]
+		head_row = HDI_sheet.row(0)
+		head_values = [cell.value for cell in head_row]
 
-for row_index in xrange(1, HDI_sheet.nrows):
-	row = HDI_sheet.row(row_index)
-	values = [cell.value for cell in row]
-	row_dict = dict(zip(head_values, values))
-	rows.append(row_dict)
+		for row_index in xrange(1, HDI_sheet.nrows):
+			row = HDI_sheet.row(row_index)
+			values = [cell.value for cell in row]
+			row_dict = dict(zip(head_values, values))
+			rows.append(row_dict)
 
-json_string = json.dumps(rows, indent = 2)
-print json_string
+	result_dict['data'] = rows
+	json_string = json.dumps(result_dict, indent = 2)
+	print json_string
+
+if __name__ == '__main__':
+	main()
