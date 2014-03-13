f42e592bd89a3a27d17edb6fe16e98073be3b7fb
Erica Pisani <pisani.erica@gmail.com>
2014-03-13 11:13:01 -0400

    Wrote test cases. Yay! \o/

--- test_infected_comp.py
+++ test_infected_comp.py
@@ -1,16 +1,15 @@
-import infected_comp
+import infected_comp as src
 import unittest
 
 class TestInfectedComputers(unittest.TestCase):
 
 	def setUp(self):
-		print ("Setup")
+		self.testFile = 'testData_antiVirus.csv'
+		self.correctResponse = 'AntiVirus_Reports: [\n{"ComputerName": "AAD-AAD77-L.CFS.UOGUELPH.CA", "Definitions Version": "1.163.1756.0", "FirstDetection": "2/1/2013 12:32", "InfectionCount": "11", "LastDetection": "29/07/2013 1:29:03 PM", "LastFullScanDateTimeEnd": "3/7/2012 14:13", "LastFullScanDateTimeStart": "3/7/2012 12:52", "LastQuickScanDateTimeEnd": "", "LastQuickScanDateTimeStart": "", "Remediation Status": "None"},\n]'
 
-	def test_importFile():
-		# test that files do import
-		# find something that asserts that a variable
-		# holds the file.
+	def test_parsing(self):
+		jsonResult = src.parseFile(self.testFile)
+		self.assertEqual(jsonResult.strip(), self.correctResponse)
 
-	def test_parsing():
-		# test that the parser is actually working
-		# assert that the JSON is correct
+if __name__ == '__main__':
+	unittest.main()
