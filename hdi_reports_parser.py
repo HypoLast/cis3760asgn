4f2b5f2a8a42b9e8e986cb7dabc87821d9948d66
Sam Wilkins <swilkins1337@gmail.com>
2014-03-21 19:47:51 -0400

    Supports multiple files via command line. Maybe improved error handling, not sure. Method actually returns a string for use by a potential caller. I think I'm supposed to make sepparted commits for this. Fuck it.

--- hdi_reports_parser.py
+++ hdi_reports_parser.py
@@ -12,36 +12,43 @@ def main():
 	if len(sys.argv) < 2:
 		sys.exit('No input file')
 	
-	filename = sys.argv[1]
+	filenames = sys.argv[1::]
 
-	parseHDI(filename)
+	for name in filenames:
+		try:
+			parseHDI(name)
+		except Exception as ex:
+			sys.stderr.write('Could not read ' + filename + ': ' + str(ex) + '\n')
 
 def parseHDI(filename):
-	HDI_book = xlrd.open_workbook(filename = filename)
+		HDI_book = xlrd.open_workbook(filename = filename)
+
+		for sheet_name in HDI_book.sheet_names():
+			parseSheet(HDI_book, sheet_name)
+
+def parseSheet(HDI_book, sheet_name):
+	HDI_sheet = HDI_book.sheet_by_name(sheet_name)
+	
 	result_dict = { 'tablename' : 'HDI_Reports',
-					'timestamp' : time.strftime('%Y:%m:%d %H:%M:%S',
-												time.localtime(time.time()))
-				  }
+		'timestamp' : time.strftime('%Y:%m:%d %H:%M:%S',
+									time.localtime(time.time()))
+		}
 	rows = list()
 
-	# I'm hoping the columns are the same in each sheet
-	for sheet_index in xrange(0, HDI_book.nsheets):
-		HDI_sheet = HDI_book.sheet_by_index(sheet_index)
-
-		head_row = HDI_sheet.row(0)
-		head_values = [cell.value for cell in head_row]
-
-		for row_index in xrange(1, HDI_sheet.nrows):
-			row = HDI_sheet.row(row_index)
-			# Need to do all this crap to output dates properly
-			values = [cell.value
-					  if cell.ctype is not xlrd.XL_CELL_DATE
-					  else datetime.datetime(*xlrd.xldate_as_tuple(
-							cell.value, HDI_book.datemode)).strftime('%Y:%m:%d')
-					  for cell in row]
-			row_dict = dict(zip(head_values, values))
-			rows.append(row_dict)
-
+	head_row = HDI_sheet.row(0)
+	head_values = [cell.value for cell in head_row]
+	
+	for row_index in xrange(1, HDI_sheet.nrows):
+		row = HDI_sheet.row(row_index)
+		# Need to do all this crap to output dates properly
+		values = [cell.value
+				  if cell.ctype is not xlrd.XL_CELL_DATE
+				  else datetime.datetime(*xlrd.xldate_as_tuple(
+															   cell.value, HDI_book.datemode)).strftime('%Y:%m:%d')
+				  for cell in row]
+		row_dict = dict(zip(head_values, values))
+		rows.append(row_dict)
+	
 	result_dict['data'] = rows
 	json_string = json.dumps(result_dict, indent = 2)
 	print json_string
