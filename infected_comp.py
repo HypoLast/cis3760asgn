15e8b609010aaece36882d0ea4115613905b071e
Erica E. Pisani-Konert <episanik@mac-2a18.socs.uoguelph.ca>
2014-03-22 12:27:19 -0400

    Fix json string for posting. Add sample test file

--- infected_comp.py
+++ infected_comp.py
@@ -4,24 +4,29 @@
 import collections
 import json
 import csv
+import time
 
 def parseFile(fileNameToParse):
 
 	# Used to skip the first row in the file when parsing
 	# for the information
 	rowCount = 0
+	jsonString = ("{'apikey' : 'b980d57f35d0dcc8508fcfb02ae27db3ffc6e5ed',\n"
+				"'datavault': 'SCCM_SCEP_Reports', \n" 
+				"'timestamp':'")
+	jsonString += str(time.strftime('%Y:%m:%d %H:%M:%S', time.localtime(time.time()))) 
+	jsonString += "',\n'data' : [\n"
 
-	jsonString = "AntiVirus_Reports: [\n"
 	# Opening in universal newline mode. Ensures backward
 	# compatibility.
-	file = open(fileNameToParse, "U")
+	fileHandle = open(fileNameToParse, "U")
 
 	fileLen = len(open(fileNameToParse, "r").readlines())
 
-	colNames = csv.reader(file)
+	colNames = csv.reader(fileHandle)
 	headings = colNames.next()
 
-	information = csv.DictReader(file, headings)
+	information = csv.DictReader(fileHandle, headings)
 	for row in information:
 		row = collections.OrderedDict(sorted(row.items()))
 		jsonString += json.dumps(row)
@@ -31,6 +36,6 @@ def parseFile(fileNameToParse):
 		else:
 			jsonString += ",\n"
 
-	jsonString += "]"
+	jsonString += "]}"
 
 	return jsonString
